0115847f5539f28636363d47d6be6ca45486d0be
mhmdhalawi <mhmdhalawi@users.noreply.github.com>
2020-02-29 04:29:51 +0200

    front end adjustments

--- src/components/ImageLinkForm/imagelinkform.jsx
+++ src/components/ImageLinkForm/imagelinkform.jsx
@@ -5,9 +5,9 @@ const ImageLinkForm = () => (
     <p className="f4 tc ">{'This magic brain will detect your faces. Give it a try.'}</p>
 
     <div className="form-parent shadow-5">
-      <div className="form w-90">
-        <input className="f5 w-70 pa2" type="text" />
-        <button className="w-20 grow f5 link pv2 ph3 dib white pointer">Detect</button>
+      <div className="form w-100">
+        <input className="f5  pa2" type="text" />
+        <button className=" grow tc f5 link pv2 ph3 dib white pointer">Detect</button>
       </div>
     </div>
   </div>
